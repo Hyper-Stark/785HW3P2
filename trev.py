b92f6b70be55a665ea355fa3a4f97e0332b5e238
lizhang <liz2@andrew.cmu.edu>
2019-04-05 16:20:24 -0400

    revise 1

--- trev.py
+++ trev.py
@@ -12,51 +12,25 @@ import torch.nn.functional as F
 
 from model import Model
 from dataset import Dataset
-from utils import details
-from utils import pad_at_end
+from utils import loader,details
 from ctc import Predictor 
 
-from data.phoneme_list import *
-from ctcdecode import CTCBeamDecoder
-from data.phoneme_list import PHONEME_MAP
  
 EPOCHS = 12
-BATCH_SIZE = 128
+TIME_ZONE = pytz.timezone("America/New_York")
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-tz = pytz.timezone("America/New_York")
+# load data
+# train_loader = loader("data/wsj0_train.npy","data/wsj0_train_merged_labels.npy")
+train_loader = loader("data/wsj0_dev.npy","data/wsj0_dev_merged_labels.npy")
+valid_loader = loader("data/wsj0_dev.npy","data/wsj0_dev_merged_labels.npy")
+
 _begin_time = time.time()
-
-#prepare data
-# train
-traindata = np.load("data/wsj0_train.npy",encoding="bytes")
-trainlabel = np.load("data/wsj0_train_merged_labels.npy",encoding="bytes")
-#traindata = np.load("data/wsj0_dev.npy",encoding="bytes")
-#trainlabel = np.load("data/wsj0_dev_merged_labels.npy",encoding="bytes")
-trainset = Dataset(traindata,trainlabel)
-# valid
-validdata = np.load("data/wsj0_dev.npy",encoding="bytes")
-validlabel = np.load("data/wsj0_dev_merged_labels.npy",encoding="bytes")
-validset = Dataset(validdata,validlabel)
-
-# test
-testdata = np.load("data/transformed_test_data.npy",encoding="bytes")
-testset = Dataset(testdata)
-
-# loaders
-train_loader = data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True, drop_last=True, collate_fn=pad_at_end, num_workers=4)
-valid_loader = data.DataLoader(validset, batch_size=BATCH_SIZE, shuffle=False, drop_last=False, collate_fn=pad_at_end, num_workers=4)
-test_loader = data.DataLoader(testset, batch_size=BATCH_SIZE, shuffle=False, drop_last=False, collate_fn=pad_at_end, num_workers=4)
-
-_end_time = time.time()
-print("load data time cost: " + str(_end_time - _begin_time))
-_begin_time = time.time()
-
 # prepare model
-model = Model(DEVICE).to(DEVICE)
+model = Model(device = DEVICE)
 criterion = nn.CTCLoss()
 opt = optim.Adam(model.parameters(),lr=0.001,weight_decay=5e-4)
-predictor = Predictor()
+# predictor = Predictor()
 
 _end_time = time.time()
 print("prepare model: " + str(_end_time - _begin_time))
@@ -68,7 +42,7 @@ for epoch in range(EPOCHS):
     model.train()
 
     #this epoch
-    nt = dt.datetime.now(tz)
+    nt = dt.datetime.now(TIME_ZONE)
     print(" ")
     print("Starting epoch "+str(epoch)+" at "+nt.strftime("%H:%M"))
     # print("current learning rate: "+str(scheduler.get_lr())) 
@@ -78,14 +52,11 @@ for epoch in range(EPOCHS):
     for voice,labels,lengths in train_loader:
        
         #clear gradients
-        print("    training batch: "+str(i))
         opt.zero_grad()
         #forward
-        output, lengths = model(voice.to(DEVICE),lengths)
-        output = output.permute(1,0,2)
+        output = model(voice)
         log_probs = output.log_softmax(2)
 
-        print("    training forward")
         #concate labels
         len_of_labels = []
         concat_labels = torch.tensor([],dtype=torch.int)
@@ -96,14 +67,13 @@ for epoch in range(EPOCHS):
         #loss
         labels = concat_labels.to(DEVICE)
         loss = criterion(log_probs, labels, tuple(lengths), tuple(len_of_labels))
+
         #accumulate loss
         nl = loss.item()
-        print("    new loss: "+str(nl))
+        print("    batch: "+str(i)+" loss: "+str(nl))
         if not math.isnan(nl):
             ave_loss += nl
-        #accumulate L distance
         
-        print("    training backward")
         #backward
         loss.backward()
         #gradient decent update
@@ -118,12 +88,9 @@ for epoch in range(EPOCHS):
     #statistic variables for validation
     vtotal = vavdis = vi = vloss = 0
     for voice, labels, lengths in valid_loader:
- 
 
-        print("    eval forward") 
-        #evaluate
-        output, lengths = model(voice.to(DEVICE),lengths)
-        output = output.permute(1,0,2)
+        # forward
+        output = model(voice)
         log_probs = output.log_softmax(2)
 
         #concate labels
@@ -137,10 +104,9 @@ for epoch in range(EPOCHS):
         labels = concat_labels.to(DEVICE)
         loss = criterion(log_probs, labels, tuple(lengths), tuple(len_of_labels))
 
-        print("    eval calculate distance") 
         #count accuracy
         nl = loss.item()
-        print("    new loss: "+str(nl))
+        print("    batch: "+str(i)+" loss: "+str(nl))
         if not math.isnan(nl):
             vloss += nl
         
@@ -148,22 +114,21 @@ for epoch in range(EPOCHS):
         vtotal += output.shape[0]
         vi += 1
 
-        '''
-
-        print("test")
-
-    '''
     _end_time = time.time()
     details((_end_time - _begin_time), ave_loss/i, avg_dis/total, vloss/vi, vavdis/vtotal)
     _begin_time = time.time()
 
     torch.save(model.state_dict(),"models/model-"+str(int(_begin_time))+str(int(vavdis/vtotal))+".pt")
 
-model.eval()
+
 result = []
+model.eval()
+test_loader = loader("data/transformed_test_data.npy")
+
+
 for voice,lengths in test_loader:
     #evaluate
-    output, lengths = model(voice.to(DEVICE),lengths)
+    output = model(voice)
     preds = predictor.predict(output)
     result = result + preds 
 
@@ -176,4 +141,3 @@ with open("result.csv",'w') as f:
     f.write("Id,Predicted\n")
     for i,item in enumerate(result):
         f.write(','.join([str(i),item]) + '\n')
-
--- utils.py
+++ utils.py
@@ -1,53 +1,93 @@
+import time
 import torch
 import numpy as np
+import torch.utils.data as data
+
+from dataset import Dataset
 from torch.nn.utils.rnn import pad_sequence
 
+BATCH_SIZE = 4
+NUM_WORKERS = 4
 
-def pad_at_end(pairs):
-
-    if len(pairs[0]) == 2:
-#        print("train mode")
-        xs,ys,sortedxs = [],[],[]
-        for i,(x,y) in enumerate(pairs):
-            xs.append(x)
-            ys.append(y)
-            sortedxs.append((x.shape[0],i))
-            
-        xs = pad_sequence(xs,batch_first=True,padding_value=0)
-        xs = xs.permute(0,2,1)
-
-        labels = [None] * len(ys)
-        result = torch.zeros(xs.shape)
-        lengths = torch.zeros(xs.shape[0], dtype=torch.int)
-        sortedxs = sorted(sortedxs, key = lambda X: X[0], reverse=True)
-        for nidx,(lenz,oidx) in enumerate(sortedxs):
-            result[nidx] = xs[oidx]
-            labels[nidx] = ys[oidx].type(torch.IntTensor) + 1
-            lengths[nidx] = lenz
+def loader(dataf,labelf=None):
+    
+    _begin_time = time.time()
+    dset = ld = None
+    chunk = np.load(dataf,encoding="bytes")
+    
+    #train mode
+    if labelf is not None:
+        labels = np.load(labelf, encoding="bytes")
+        dset = Dataset(chunk,labels)
         
-        return result,labels,lengths
-
+        if torch.cuda.is_available():
+            ld = data.DataLoader(\
+                dset, \
+                batch_size=BATCH_SIZE, \
+                shuffle=True, \
+                drop_last=True, \
+                collate_fn=collate_train, \
+                num_workers=NUM_WORKERS)
+        else:
+            ld = data.DataLoader(\
+                dset, \
+                batch_size=BATCH_SIZE, \
+                shuffle=True, \
+                drop_last=True, \
+                collate_fn=collate_train)
+    #test mode
     else:
+        dset = Dataset(chunk)
+        if torch.cuda.is_available():
+            ld = data.DataLoader(\
+                dset, \
+                batch_size=BATCH_SIZE, \
+                shuffle=False, \
+                drop_last=False, \
+                collate_fn=collate_test, \
+                num_workers=NUM_WORKERS)
+        else:
+            ld = data.DataLoader(\
+                dset, \
+                batch_size=BATCH_SIZE, \
+                shuffle=False, \
+                drop_last=False, \
+                collate_fn=collate_test)
+    
+    _end_time = time.time()
+    print("load data time cost: " + str(_end_time - _begin_time))
+    return ld
 
-#        print("eval mode")
-        xs,sortedxs = [],[]
-        for i,x in enumerate(pairs):
-            xs.append(x)
-            sortedxs.append((x.shape[0],i))
-            
-        xs = pad_sequence(xs,batch_first=True,padding_value=0)
-        xs = xs.permute(0,2,1)
+def collate_train(pairs):
 
-        result = torch.zeros(xs.shape)
-        lengths = torch.zeros(xs.shape[0], dtype=torch.int)
-        sortedxs = sorted(sortedxs, key = lambda X: X[0], reverse=True)
-        for nidx,(lenz,oidx) in enumerate(sortedxs):
-            result[nidx] = xs[oidx]
-            lengths[nidx] = lenz
-        
-        return result,lengths
+    #split column
+    inputs, labels = zip(*pairs)
+    #collect lengths data
+    seqlens = [(seq.shape[0], i) for i,seq in enumerate(inputs)]
+    #sort indices according descending lengths
+    sorted_seqlens = sorted(seqlens, key=lambda x: x[0], reverse=True)
 
-       
+    seqs,labs,lens = [],[],[]
+    for lenz, oidx in sorted_seqlens:
+        lens.append(lenz)
+        seqs.append(inputs[oidx])
+        labs.append(labels[oidx].type(torch.IntTensor) + 1)
+
+    return seqs,labs,lens
+
+def collate_test(inputs):
+
+    #collect lengths data
+    seqlens = [(seq.shape[0], i) for i,seq in enumerate(inputs)]
+    #sort indices according descending lengths
+    sorted_seqlens = sorted(seqlens, key=lambda x: x[0], reverse=True)
+
+    seqs,lens = [],[]
+    for lenz, oidx in sorted_seqlens:
+        lens.append(lenz)
+        seqs.append(inputs[oidx])
+
+    return seqs,lens
 
 def details(t,loss,acc,vloss,vacc):
     print("time cost:"+str(t/60)+" mins")
@@ -55,6 +95,3 @@ def details(t,loss,acc,vloss,vacc):
     print("train ave_Loss: "+str(loss)+" | average distance: "+str(acc))
     print(" ")
     print("---------------------------------------")
-
-
-    
